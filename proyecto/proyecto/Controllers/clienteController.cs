d5586c62a6c304d8da1428ff5f7edf064b701635
rapsodi <ramstein_3000@hotmail.com>
2012-09-29 00:16:50 -0400

    envio

---
+++ proyecto/proyecto/Controllers/clienteController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using proyecto.Models;
+
+namespace proyecto.Controllers
+{
+    public class clienteController : Controller
+    {
+        //
+        // GET: /cliente/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ proyecto/proyecto/Models/cliente.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace proyecto.Models
+{
+    public class cliente
+    {
+        [Required]
+        [Display(Name = "Nombre : ")]
+        public string Nombre { get; set; }
+        [Required]
+        [Display(Name = "Ap. Pat. : ")]
+        public string Ap{ get; set; }
+        [Required]
+        [Display(Name = "Ap. Mat. : ")]
+        public string Am { get; set; }
+        [Required]
+        [Display(Name = "Documento de Identidad : ")]
+        public string Di { get; set; }
+        [Required]
+        [Display(Name = "Tipo de Documento : ")]
+        public string Td { get; set; }
+        [Required ]
+        [Display(Name = "Pais : ")]
+        public string pa { get; set; }
+        [Required]
+        [Display(Name = "Ciudad : ")]
+        public string ciu { get; set; }
+        [Required]
+        [Display(Name = "Dirección : ")]
+        public string dir { get; set; }
+        [Required]
+        [Display(Name = "Email : ")]
+        public string e { get; set; }
+        [Required]
+        [Display(Name = "Telefono : ")]
+        public int t { get; set; }
+        [Required]
+        [Display(Name = "Fecha de Nacimiento : ")]
+        public string fn { get; set; }
+    }
+}
